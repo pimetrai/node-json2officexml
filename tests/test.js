7bb0bea7f5b5bebd79e9ca423f110790068aca5c
Pierre Metrailler <pierre@spotme.com>
2012-04-06 13:33:09 +0200

    Initial import

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+var path = require("path");
+var fs = require("fs");
+var assert = require("assert");
+var js2o = require("../");
+
+var writer = js2o.createExcelOfficeXmlWriter();
+
+var doc1 = {
+    columns : [],
+    rows : [
+        { firstname : "John", lastname: "Doo"},
+        { firstname : "Foo", lastname: "Bar", age: "23"}
+    ]
+    
+};
+
+console.log(writer.writeDoc(doc1).toString({ pretty: true }));
