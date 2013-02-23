38dc4675017ad445913d47e7e036f141f8d0143c
Pierre Metrailler <pierre@shockfish.com>
2013-02-23 21:46:16 +0100

    multi-sheet format. Backward compat with previous format.

--- tests/test.js
+++ tests/test.js
@@ -14,4 +14,26 @@ var doc1 = {
     
 };
 
+var doc2 = {
+	"sheets" : [
+		{
+			"name" : "Feuil1",
+	    	"columns" : [],
+	    	"rows" : [
+	        	{ "firstname" : "John", "lastname": "Doo"},
+	        	{ "firstname" : "Foo", "lastname": "Bar", "age": 23, "weight": 25.7876, "birth" : new Date()}
+	    	]
+    	},
+    	{
+			"name" : "Feuil2",
+	    	"columns" : [],
+	    	"rows" : [
+	        	{ "firstname" : "Rene", "lastname": "Malin"},
+	        	{ "firstname" : "Foo", "lastname": "foobar", "age": 73, "weight": 22225.33, "birth" : new Date()}
+	    	]
+    	}
+    ]
+};
+
 console.log(writer.writeDoc(doc1).toString({ pretty: true }));
+console.log(writer.writeDoc(doc2).toString({ pretty: true }));
